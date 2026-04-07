12b062f7f91bb349c8dc57981f7925a555e78e28
guilhermesegovia <guilhermemachadosegovia@gmail.com>
2026-04-07 19:18:11 -0300

    fiz os imports do controller no server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from "express";
 
+import { CategoriasController } from "./controllers/categoriascontroller";
 import { ClienteController } from "./controllers/ClienteController";
+import { EstoqueController } from "./controllers/EstoqueController";
+import { FornecedorController } from "./controllers/Fornecedorescontroller";
+import { ItemPedidoController } from "./controllers/itenspedidoscontroller";
+import { PagamentoController } from "./controllers/pagamentosconroller";
+import { PedidoController } from "./controllers/pedidoscontroller";
 import { ProdutoController } from "./controllers/ProdutoController";
-import { VendaController } from "./controllers/VendaController";
+import { UsuarioAdminController } from "./controllers/usuarioadminController";
 
 export const app = express();
 
@@ -10,7 +16,13 @@ app.use(express.json());
 
 ClienteController();
 ProdutoController();
-VendaController();
+CategoriasController();
+EstoqueController();
+FornecedorController();
+ItemPedidoController();
+PagamentoController();
+PedidoController();
+UsuarioAdminController();
 
 app.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000");
